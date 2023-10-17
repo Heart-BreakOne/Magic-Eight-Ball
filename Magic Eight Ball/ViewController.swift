93f59a0c344c6f347f83b044eefaeef88dd5054a
Leonardo Luiz <lcoutinholuiz@gmail.com>
2023-10-17 02:08:32 -0300

    Commiting to github

--- Magic Eight Ball/ViewController.swift
+++ Magic Eight Ball/ViewController.swift
@@ -4,11 +4,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Declares an array of ball images
+    let arrayOfBalls = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
+    
+    //Declares the image view outlet so the image can be set
+    @IBOutlet weak var ImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    //IBAction for when the user clicks the button, then sets imageView to a random image
+    @IBAction func askButtonPressed(_ sender: Any) {
+        //ImageView.image = UIImage.ball2
+        //ImageView.image = arrayOfBalls[Int.random(in: 0...4)]
+       
+        //Generates random number
+        let randomNumber = Int.random(in: 0...4);
+        //Sets image view with an random image from the array.
+        ImageView.image = arrayOfBalls[randomNumber];
+    }
 }
-
---
+++ README.md
@@ -0,0 +1,5 @@
+# Magic Eight Ball storyboard swift app
+## Randomly shows a eight ball image answer when user presses button
+<p align="center">
+<img src="https://i.imgur.com/JA7smR9.png" alt="Magic Eight Ball app" height="780px" width="360px"/>
+</p>
